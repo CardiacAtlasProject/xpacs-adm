61092654bba470fe7366c79a86b210ee58ffa6a1
Avan Suinesiaputra <avan.sp@gmail.com>
2016-10-04 12:56:16 +1300

    Initial data-test.sql to populate testing data

---
+++ src/main/resources/sql/data-test.sql
@@ -0,0 +1,4 @@
+/* INSERT QUERIES TO PATIENT_INFO */
+INSERT INTO xpacs.PATIENT_INFO(PACS_patient_id,Age,Gender,Ethnicity,Cohort,PrimaryDiagnosis) VALUES( 'PAT0001',28,'F','Asian','CHD','Fontan, Coarc');
+INSERT INTO xpacs.PATIENT_INFO(PACS_patient_id,Age,Gender,Ethnicity,Cohort,PrimaryDiagnosis) VALUES( 'PAT0005',22,'M','Caucasian','CHD','Coarc');
+INSERT INTO xpacs.PATIENT_INFO(PACS_patient_id,Age,Gender,Ethnicity,Cohort,PrimaryDiagnosis) VALUES( 'PAT0002',13,'M','Caucasian','CHD','Tricuspid Atresia');
